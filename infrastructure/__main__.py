1a6090a0857f6c1f1ec619af8cd617c303892076
Kirin Patel <kirinpatel@gmail.com>
2024-06-15 20:32:13 -0400

    🔧 format and update service plan SKU S1 -> Y1

--- infrastructure/__main__.py
+++ infrastructure/__main__.py
@@ -5,20 +5,20 @@ import pulumi_azure as azure
 
 resource_group = azure.core.ResourceGroup("resource-group", location="eastus")
 storage_account = azure.storage.Account(
-	"storage-account",
-	resource_group_name=resource_group.name,
-	location=resource_group.location,
-	account_tier="Standard",
-	account_replication_type="LRS")
+    "storage-account",
+    resource_group_name=resource_group.name,
+    location=resource_group.location,
+    account_tier="Standard",
+    account_replication_type="LRS")
 service_plan = azure.appservice.ServicePlan(
-	"service-plan",
+    "service-plan",
     resource_group_name=resource_group.name,
     location=resource_group.location,
     os_type="Linux",
-    sku_name="B1")
+    sku_name="Y1")
 functionApp = azure.appservice.LinuxFunctionApp(
-	"function-app",
-	resource_group_name=resource_group.name,
+    "function-app",
+    resource_group_name=resource_group.name,
     location=resource_group.location,
     storage_account_name=storage_account.name,
     storage_account_access_key=storage_account.primary_access_key,
